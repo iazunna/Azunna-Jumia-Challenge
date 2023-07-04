8b3b9951239f115a1c8f3c05999deb1710d2693f
Azunna Ikonne <azunna.ikonne@deimos.co.za>
2023-07-04 20:33:12 +0100

    update backend

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "jumia-challenge-tf-bucket"
     key = "jumia-challenge/terraform.tfstate"
     encrypt = true
-    region = local.region
+    region = "eu-west-2"
     dynamodb_table = "terraform-locks"
   }
 }
