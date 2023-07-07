d25e7d3cfbd5eb81158096a19490f4372342849f
Azunna Ikonne <azunna.ikonne@deimos.co.za>
2023-07-07 18:45:57 +0100

    update provider config
    Signed-off-by: Azunna Ikonne <azunna.ikonne@deimos.co.za>

---
+++ infrastructure/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+    helm = {
+      source = "hashicorp/helm"
+      version = ">= 2.10.1"
+    }
+  }
+}
