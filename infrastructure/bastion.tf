04d54058d616d082fbce97fd24377f076d082f53
Azunna Ikonne <azunna.ikonne@deimos.co.za>
2023-07-09 11:16:21 +0100

    Update ECR config
    Signed-off-by: Azunna Ikonne <azunna.ikonne@deimos.co.za>

--- infrastructure/bastion.tf
+++ infrastructure/bastion.tf
@@ -78,7 +78,7 @@ resource "aws_instance" "bastion" {
   subnet_id                   = module.vpc.public_subnets[0]
   vpc_security_group_ids      = [aws_security_group.bastion.id]
   associate_public_ip_address = true
-  key_name = module.key_pair_bastion.key_pair_name
+  key_name                    = module.key_pair_bastion.key_pair_name
 
   root_block_device {
     volume_size = 20
--- infrastructure/ecr.tf
+++ infrastructure/ecr.tf
@@ -1,9 +1,9 @@
-module "ecr_registry" {
+module "ecr_repos" {
 
   for_each = toset(local.repositories)
-  source = "terraform-aws-modules/ecr/aws"
+  source   = "terraform-aws-modules/ecr/aws"
 
-  repository_name = each.value
+  repository_name                 = each.value
   repository_image_tag_mutability = "IMMUTABLE"
   repository_lifecycle_policy = jsonencode({
     rules = [
@@ -23,10 +23,16 @@ module "ecr_registry" {
     ]
   })
 
+}
+
+module "ecr_registry" {
+
+  source            = "terraform-aws-modules/ecr/aws"
+  create_repository = false
   # Registry Pull Through Cache Rules
   registry_pull_through_cache_rules = {
     pub = {
-      ecr_repository_prefix = "ecr-public-${each.key}"
+      ecr_repository_prefix = "ecr-public"
       upstream_registry_url = "public.ecr.aws"
     }
   }
