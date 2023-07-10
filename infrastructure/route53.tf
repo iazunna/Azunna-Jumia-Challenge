e2d8fb41f2d20b4d11fa41ad8446f1c2531e7231
Azunna Ikonne <azunna.ikonne@deimos.co.za>
2023-07-10 12:22:34 +0100

    update dnssec
    Signed-off-by: Azunna Ikonne <azunna.ikonne@deimos.co.za>

--- infrastructure/route53.tf
+++ infrastructure/route53.tf
@@ -17,60 +17,61 @@ resource "aws_route53_record" "devops-ns" {
   records = aws_route53_zone.devops.name_servers
 }
 
-# resource "aws_kms_key" "jumia_zone_key" {
-#   customer_master_key_spec = "ECC_NIST_P256"
-#   deletion_window_in_days  = 7
-#   key_usage                = "SIGN_VERIFY"
-#   policy = jsonencode({
-#     Statement = [
-#       {
-#         Action = [
-#           "kms:DescribeKey",
-#           "kms:GetPublicKey",
-#           "kms:Sign",
-#         ],
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "dnssec-route53.amazonaws.com"
-#         }
-#         Sid      = "Allow Route 53 DNSSEC Service",
-#         Resource = "*"
-#         Condition = {
-#           StringEquals = {
-#             "aws:SourceAccount" = data.aws_caller_identity.current.account_id
-#           }
-#           ArnLike = {
-#             "aws:SourceArn" = "arn:aws:route53:::hostedzone/*"
-#           }
-#         }
-#       },
-#       {
-#         Action = "kms:CreateGrant",
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "dnssec-route53.amazonaws.com"
-#         }
-#         Sid      = "Allow Route 53 DNSSEC Service to CreateGrant",
-#         Resource = "*"
-#         Condition = {
-#           Bool = {
-#             "kms:GrantIsForAWSResource" = "true"
-#           }
-#         }
-#       },
-#       {
-#         Action = "kms:*"
-#         Effect = "Allow"
-#         Principal = {
-#           AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-#         }
-#         Resource = "*"
-#         Sid      = "Enable IAM User Permissions"
-#       },
-#     ]
-#     Version = "2012-10-17"
-#   })
-# }
+resource "aws_kms_key" "jumia_zone_key" {
+  region = "us-east-1"
+  customer_master_key_spec = "ECC_NIST_P256"
+  deletion_window_in_days  = 7
+  key_usage                = "SIGN_VERIFY"
+  policy = jsonencode({
+    Statement = [
+      {
+        Action = [
+          "kms:DescribeKey",
+          "kms:GetPublicKey",
+          "kms:Sign",
+        ],
+        Effect = "Allow"
+        Principal = {
+          Service = "dnssec-route53.amazonaws.com"
+        }
+        Sid      = "Allow Route 53 DNSSEC Service",
+        Resource = "*"
+        Condition = {
+          StringEquals = {
+            "aws:SourceAccount" = data.aws_caller_identity.current.account_id
+          }
+          ArnLike = {
+            "aws:SourceArn" = "arn:aws:route53:::hostedzone/*"
+          }
+        }
+      },
+      {
+        Action = "kms:CreateGrant",
+        Effect = "Allow"
+        Principal = {
+          Service = "dnssec-route53.amazonaws.com"
+        }
+        Sid      = "Allow Route 53 DNSSEC Service to CreateGrant",
+        Resource = "*"
+        Condition = {
+          Bool = {
+            "kms:GrantIsForAWSResource" = "true"
+          }
+        }
+      },
+      {
+        Action = "kms:*"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Resource = "*"
+        Sid      = "Enable IAM User Permissions"
+      },
+    ]
+    Version = "2012-10-17"
+  })
+}
 
 
 resource "aws_route53_key_signing_key" "jumia_zone_key" {
