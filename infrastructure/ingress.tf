d478c1d3307f5c241675f06a4dbef245b3536c2f
Azunna Ikonne <azunna.ikonne@deimos.co.za>
2023-07-10 19:42:20 +0100

    Install nginx ingress
    Signed-off-by: Azunna Ikonne <azunna.ikonne@deimos.co.za>

---
+++ infrastructure/ingress.tf
@@ -0,0 +1,65 @@
+resource "helm_release" "ingress" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  version    = "1.5.4"
+  namespace  = "kube-system"
+  atomic     = true
+
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "alb-controller-sa"
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.alb_controller_irsa.iam_role_arn
+  }
+
+  depends_on = [
+    module.eks
+  ]
+}
+
+resource "helm_release" "ingress_nginx" {
+  name       = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  version    = "4.7.1"
+  namespace  = "ingress-nginx"
+  atomic     = true
+
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-cert"
+    value = "arn:aws:acm:us-east-1:992122884453:certificate/99876741-2cbb-4575-8a42-ee3aad6a6018"
+  }
+
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-backend-protocol"
+    value = "http"
+  }
+
+    set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-ports"
+    value = "https"
+  }
+
+  set {
+    name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-connection-idle-timeout"
+    value = "3600"
+  }
+
+  depends_on = [
+    module.eks
+  ]
+}
