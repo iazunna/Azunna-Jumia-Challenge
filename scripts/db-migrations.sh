cd627e9d89b8327d5e23f42d5debda465216613e
Azunna Ikonne <azunna.ikonne@deimos.co.za>
2023-07-10 17:59:52 +0100

    update alb annotations
    Signed-off-by: Azunna Ikonne <azunna.ikonne@deimos.co.za>

--- scripts/db-migrations.sh
+++ scripts/db-migrations.sh
@@ -5,6 +5,6 @@ set -u
 PGPASSWORD="$DB_PASSWORD" psql -h "$DB_HOST" -d "$DB_NAME" -U "$DB_USER" -p 5432 -a -q -f $@
 
 # BEGIN TRANSACTION;
-# CREATE USER db-user WITH PASSWORD password;
+# CREATE ROLE db-user WITH PASSWORD password;
 # GRANT ALL ON DATABASE db TO db-user;
 # COMMIT;
