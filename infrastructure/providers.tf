32f2b512cbeb0303992005df73a16606f7cc9cca
Azunna Ikonne <azunna.ikonne@deimos.co.za>
2023-07-10 08:06:44 +0100

    Use token for kubectl
    Signed-off-by: Azunna Ikonne <azunna.ikonne@deimos.co.za>

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,3 +1,9 @@
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+
+
 provider "aws" {
   region = local.region
   default_tags {
@@ -21,12 +27,13 @@ provider "kubectl" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   load_config_file       = false
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-  }
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  # exec {
+  #   api_version = "client.authentication.k8s.io/v1beta1"
+  #   command     = "aws"
+  #   # This requires the awscli to be installed locally where Terraform is executed
+  #   args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  # }
 }
 
 provider "helm" {
