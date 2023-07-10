7d3fe68fed842ff2986c3cc810b3194646a0c80e
Azunna Ikonne <azunna.ikonne@deimos.co.za>
2023-07-10 12:36:14 +0100

    update dnssec provider
    Signed-off-by: Azunna Ikonne <azunna.ikonne@deimos.co.za>

--- infrastructure/route53.tf
+++ infrastructure/route53.tf
@@ -73,6 +73,62 @@ resource "aws_kms_key" "jumia_zone_key" {
   })
 }
 
+resource "aws_kms_key" "dev_ops_jumia_zone_key" {
+  provider = aws.kms_dnssec
+  customer_master_key_spec = "ECC_NIST_P256"
+  deletion_window_in_days  = 7
+  key_usage                = "SIGN_VERIFY"
+  policy = jsonencode({
+    Statement = [
+      {
+        Action = [
+          "kms:DescribeKey",
+          "kms:GetPublicKey",
+          "kms:Sign",
+        ],
+        Effect = "Allow"
+        Principal = {
+          Service = "dnssec-route53.amazonaws.com"
+        }
+        Sid      = "Allow Route 53 DNSSEC Service",
+        Resource = "*"
+        Condition = {
+          StringEquals = {
+            "aws:SourceAccount" = data.aws_caller_identity.current.account_id
+          }
+          ArnLike = {
+            "aws:SourceArn" = "arn:aws:route53:::hostedzone/*"
+          }
+        }
+      },
+      {
+        Action = "kms:CreateGrant",
+        Effect = "Allow"
+        Principal = {
+          Service = "dnssec-route53.amazonaws.com"
+        }
+        Sid      = "Allow Route 53 DNSSEC Service to CreateGrant",
+        Resource = "*"
+        Condition = {
+          Bool = {
+            "kms:GrantIsForAWSResource" = "true"
+          }
+        }
+      },
+      {
+        Action = "kms:*"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Resource = "*"
+        Sid      = "Enable IAM User Permissions"
+      },
+    ]
+    Version = "2012-10-17"
+  })
+}
+
 
 resource "aws_route53_key_signing_key" "jumia_zone_key" {
   hosted_zone_id             = aws_route53_zone.jumia-challenge.id
@@ -80,10 +136,25 @@ resource "aws_route53_key_signing_key" "jumia_zone_key" {
   name                       = "jumiachallengezonekey"
 }
 
-resource "aws_route53_hosted_zone_dnssec" "example" {
+resource "aws_route53_hosted_zone_dnssec" "jumia-challenge" {
   depends_on = [
     aws_route53_key_signing_key.jumia_zone_key
   ]
   hosted_zone_id = aws_route53_key_signing_key.jumia_zone_key.hosted_zone_id
 }
+
+resource "aws_route53_key_signing_key" "devops_jumia_zone_key" {
+  hosted_zone_id             = aws_route53_zone.jumia-challenge.id
+  key_management_service_arn = aws_kms_key.dev_ops_jumia_zone_key
+  name                       = "jumiachallengezonekey"
+}
+
+resource "aws_route53_hosted_zone_dnssec" "jumia-challenge" {
+  depends_on = [
+    aws_route53_key_signing_key.devops_jumia_zone_key
+  ]
+  hosted_zone_id = aws_route53_key_signing_key.devops_jumia_zone_key.hosted_zone_id
+}
+
+
 
