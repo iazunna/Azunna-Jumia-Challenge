b26a28ba2d2a1bce05e7fbf6a88e6cbc19cdd6b4
Azunna Ikonne <azunna.ikonne@deimos.co.za>
2023-07-11 09:33:38 +0100

    Update dns servers on cert manager
    Signed-off-by: Azunna Ikonne <azunna.ikonne@deimos.co.za>

--- infrastructure/cert-manager.tf
+++ infrastructure/cert-manager.tf
@@ -1,6 +1,6 @@
 
 locals {
-  zone_recursive_ns = join(":53,", aws_route53_zone.jumia_challenge.name_servers)
+  zone_recursive_ns = join("\\:53\\,", aws_route53_zone.jumia_challenge.name_servers)
 }
 resource "helm_release" "cert_manager" {
   name             = "cert-manager"
@@ -34,7 +34,7 @@ resource "helm_release" "cert_manager" {
 
   set {
     name = "extraArgs[2]"
-    value = "\"--dns01-recursive-nameservers=8.8.8.8:53,1.1.1.1:53,${local.zone_recursive_ns}:53\""
+    value = "\"--dns01-recursive-nameservers=8.8.8.8\\:53\\,1.1.1.1\\:53\\,${local.zone_recursive_ns}\\:53\""
   }
 
   set {
